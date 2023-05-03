e2a14c43ce49d011b279712716dfd7a6bf79751a
kashivirus <kashivirus691@gmail.com>
2023-05-03 22:19:48 +0500

    asd

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
-const port = 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json([
+    {
+      "id": "1",
+      "title": "This is my First Blog Title"
+    },
+    {
+      "id": "2",
+      "title": "This is my Second Blog Title"
+    },
+    {
+      "id": "3",
+      "title": "This is my Third Blog Title"
+    },
+    {
+      "id": "4",
+      "title": "This is my Fourth Blog Title"
+    },
+    {
+      "id": "5",
+      "title": "This is my Fifth Blog Title"
+    }
+  ])
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(4000, () => {
+  console.log('Server running on port 4000')
 })
